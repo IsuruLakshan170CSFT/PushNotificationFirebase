f0c8bf5b166e63725d26c11ffadcfee49f4d3b5a
Isuru <lakshanisuru170@gmail.com>
2022-05-26 23:36:02 +0530

    Init

---
+++ lib/main.dart
@@ -0,0 +1,138 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_push_notification/services/local_notification_service.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+import 'dart:io' show Platform;
+
+//receive message when app is in background solution for on message
+Future<void> backgroundHandler(RemoteMessage message) async {
+  print(message.data.toString());
+  print(message.notification!.title);
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    LocalNotificationService.initialize(context);
+    sendToken();
+    //gives you the message on which user taps
+    //and it opend the app from terminated state
+    FirebaseMessaging.instance.getInitialMessage().then((message) {
+      if (message != null) {
+        final routeFromMessage = message.data["route"];
+        Navigator.of(context).pushNamed(routeFromMessage);
+      }
+    });
+
+    //forgound work
+    FirebaseMessaging.onMessage.listen((message) {
+      if (message.notification != null) {
+        print(message.notification!.body);
+        print(message.notification!.title);
+      }
+      LocalNotificationService.display(message);
+    });
+
+    //works at user on tap , app is in background not open or not open but in background
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      final routeFromMessage = message.data["route"];
+      Navigator.of(context).pushNamed(routeFromMessage);
+      print(routeFromMessage);
+    });
+  }
+
+  Future<void> sendToken() async {
+    String? token;
+    String? device;
+
+    print("test init state");
+    FirebaseMessaging.instance.getToken().then((value) async {
+      String? token = value;
+      if (Platform.isAndroid) {
+        device = "Android Application";
+        // Android-specific code
+      } else if (Platform.isIOS) {
+        device = "IOS Application";
+        // iOS-specific code
+      }
+      print(device);
+
+      print(token);
+      var url = Uri.parse(
+          'https://b4kwc0wdh6.execute-api.us-east-1.amazonaws.com/addOne');
+      try {
+        final bodyObj = {
+          'user': 'android',
+          'token': token,
+          'deviceType': device
+        };
+        var response = await http.post(
+          url,
+          body: bodyObj,
+        );
+        print('Response status: ${response.statusCode}');
+        print('Response body: ${response.body}');
+      } catch (e) {
+        print(e);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Flutter Push notification ',
+            ),
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
